eb3a7928c6c1bf06830011a9ecb82a84f30853d8
crystalsky14 <crystalsky14@gmail.com>
2016-07-17 17:41:36 -0400

    movents
    movements

---
+++ movements/Assets/scripts/unstuck.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class unstuck : MonoBehaviour
+{
+    public int collisioncount=0 ;
+    private float constantup = 0.05f;
+    public GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+        collisioncount = -3; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collisioncount > 0 )
+        {
+            player.transform.Translate(Vector3.up * constantup);
+            movementplayer.once = false;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        collisioncount++;
+
+    }
+    void OnTriggerExit(Collider other)
+    {
+        collisioncount--;
+    }
+}
